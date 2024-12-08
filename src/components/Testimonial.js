2cfb080c938788e0fd4af0249dc8dcc7333a161c
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-09 02:35:03 +0530

    this is my two hundred ninty commit

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -36,7 +36,7 @@ const Testimonial = (props) => {
             <button onClick={leftShiftHandler} className="cursor-pointer hover:text-violet-500"><FiChevronLeft/></button>
             <button onClick={rightShiftHandler} className="cursor-pointer hover:text-violet-500"><FiChevronRight/></button>
         </div>
-        <div>
+        <div className="mt-6">
             <button onClick={surpriseClickHandler} className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
                 Surprise me
             </button>
