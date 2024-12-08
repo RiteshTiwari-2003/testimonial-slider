523d87a697617ad4f3f47d5b5d13c0106d1a0398
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-09 00:36:58 +0530

    this is my slider 3 commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import {FaQuoteLeft,FaQuoteRight} from "react-icons/fa";
 const Card = (props) => {
     let review=props.review;
   return (
-    <div>
-        <div>
-            <img src={review.image}/>
+    <div className='flex flex-col md:relative'>
+        <div className="absolute top-[-7rem] z-[10] mx-auto">
+            <img className="aspect-square rounded-full w-[140px] h-[140px] z-25" src={review.image}/>
         </div>
         <div>
             <p>{review.name}</p>
